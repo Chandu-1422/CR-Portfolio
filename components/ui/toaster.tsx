8b8640e4bd4152db5b49c0922307cf3743dedbc6
unknown <picaseo1820@gmail.com>
2025-07-23 13:47:11 +0530

    main

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -8,7 +8,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
+} from "@/my-portfolio/components/ui/toast"
 
 export function Toaster() {
   const { toasts } = useToast()
